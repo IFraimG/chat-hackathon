09790e5536e828f1c051cb93d6bfccf34afa53da
xxRexetxx <egorvereykin2@gmail.com>
2023-04-13 12:32:24 +0300

    1

--- app/src/main/java/com/example/todolisthwhackathon/MainActivity.java
+++ app/src/main/java/com/example/todolisthwhackathon/MainActivity.java
@@ -4,12 +4,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.todolisthwhackathon.adapters.ViewPagerAdapter;
+import com.example.todolisthwhackathon.fragments.Page1;
+import com.example.todolisthwhackathon.fragments.Page2;
+import com.example.todolisthwhackathon.fragments.Page3;
+import com.example.todolisthwhackathon.fragments.Page4;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPagerAdapter viewPagerAdapter;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        viewPager = findViewById(R.id.viewpager);
+
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+
+        viewPagerAdapter.add(new Page1(), "Журналы");
+        viewPagerAdapter.add(new Page2(), "Графики");
+        viewPagerAdapter.add(new Page3(), "Заметки");
+        viewPagerAdapter.add(new Page4(), "Настройка");
+
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+
+        tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
