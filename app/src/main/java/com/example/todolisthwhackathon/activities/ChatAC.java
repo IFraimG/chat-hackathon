3af3a1122b390380bb36e16155252a161509f4f7
truesnickers <egorvereykin2@gmail.com>
2023-04-16 12:16:23 +0300

    firebaseSaving4

--- app/src/main/java/com/example/todolisthwhackathon/activities/ChatAC.java
+++ app/src/main/java/com/example/todolisthwhackathon/activities/ChatAC.java
@@ -1,6 +1,8 @@
 package com.example.todolisthwhackathon.activities;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,15 @@ import com.example.todolisthwhackathon.data.api.CustomFirebase;
 
 public class ChatAC extends AppCompatActivity {
 
+    EditText messageText;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        messageText = findViewById(R.id.messagetext);
 
         if (getIntent() != null) {
 //            String chatID = getIntent().getStringExtra("chatID");
