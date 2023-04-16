10b7bd79d0033dd98b88bcebe2a549c3b513c323
truesnickers <egorvereykin2@gmail.com>
2023-04-16 14:35:33 +0300

    firebaseSaving6

--- app/src/main/java/com/example/todolisthwhackathon/fragments/Fragment_Chat.java
+++ app/src/main/java/com/example/todolisthwhackathon/fragments/Fragment_Chat.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -39,6 +40,8 @@ import java.util.List;
 public class Fragment_Chat extends Fragment {
     ChatListViewModel viewModel;
     String id;
+
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -93,6 +96,10 @@ public class Fragment_Chat extends Fragment {
         });
 
 
+
+
+
+
         joinChat.setOnClickListener(View -> {
             FirebaseDatabase.getInstance().getReference().child("chats").addValueEventListener(new ValueEventListener() {
                     @Override
@@ -125,5 +132,7 @@ public class Fragment_Chat extends Fragment {
                 });
         });
         return view;
+
+
     }
 }
